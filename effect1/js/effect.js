79149b38238c04c246ddec8416ca2f5775f94cee
yinchuandong <yincd520@sina.com>
2013-11-14 11:06:19 +0800

    f

--- effect1/js/effect.js
+++ effect1/js/effect.js
@@ -2,17 +2,17 @@ window.onload = function(){
 	var tag = getElementsByClassName('skitter')[0];
 	var list = [];
 	for(var i=0; i<9;i++){
-		var elem = createBlock({left:i*100});
-		list.push(elem);
-		// tag.appendChild(elem);
+		var elem = createBlock({left:i*100,top:-510});
+		// list.push(elem);
+		tag.appendChild(elem);
 	}
 	doMovement(list,0);
 }
 
 function doMovement(list,curIndex){
-	var tag = getElementsByClassName('skitter')[0];
-	tag.appendChild(list[curIndex]);
+	var elem = list[curIndex];
 	curIndex ++;
+	startMove(elem,{top:0},100);
 	var timer = setTimeout(function(){
 		if (curIndex == 9) {
 			debug(curIndex);
